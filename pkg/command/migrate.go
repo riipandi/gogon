d8b4a888e1eee53a25706951a441a378b9d48a92
Aris Ripandi <riipandi@gmail.com>
2022-12-10 18:10:30 +0700

    add migrate subcommand

---
+++ pkg/command/migrate.go
@@ -0,0 +1,19 @@
+package command
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+var migrateCmd = &cobra.Command{
+	Use:   "migrate",
+	Short: "Run the database migratons",
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("Not yet implemented!")
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(migrateCmd)
+}
