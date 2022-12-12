d1f2600193b76a92e18ded9643c7a39243353e79
Aris Ripandi <riipandi@gmail.com>
2022-12-13 05:57:25 +0700

    fix bun request error handler

--- pkg/handler/errors.go
+++ pkg/handler/errors.go
@@ -7,6 +7,16 @@ import (
 	"github.com/uptrace/bunrouter"
 )
 
+func NotFoundHandler(w http.ResponseWriter, req bunrouter.Request) error {
+	w.WriteHeader(http.StatusNotFound)
+	fmt.Fprintf(
+		w,
+		"<html>BunRouter can't find a route that matches <strong>%s</strong></html>",
+		req.URL.Path,
+	)
+	return nil
+}
+
 func MethodNotAllowedHandler(w http.ResponseWriter, req bunrouter.Request) error {
 	w.WriteHeader(http.StatusMethodNotAllowed)
 	fmt.Fprintf(
